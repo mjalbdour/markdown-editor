c47bb9ef76cac420be1a311e77b1c1cc135904be
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-17 23:48:32 +0300

    Stage 1/5: Meet the markdown

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -1,1 +1,13 @@
 # write your code here
+markdown = """
+# John Lennon
+or ***John Winston Ono Lennon*** was one of *The Beatles*.
+Here are the songs he wrote I like the most:
+* Imagine
+* Norwegian Wood
+* Come Together
+* In My Life
+* ~~Hey Jude~~ (that was *McCartney*)
+"""
+
+print(markdown)
