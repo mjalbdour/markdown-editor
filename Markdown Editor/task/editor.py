8e55c227342d739a0b7972520e1e83473d3a31af
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-18 20:50:00 +0300

    Stage 4/5: Ordered and unordered lists

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -6,25 +6,32 @@ msg_text = "Text: "
 msg_level = "Level: "
 msg_label = "Label: "
 msg_url = "URL: "
+msg_row = "Row "
+msg_rows = "Number of rows: "
 msg_unknown_formatting = "Unknown formatting type or command"
 msg_level_error = "The level should be within the range of 1 to 6"
-
-option_help = "!help"
-option_done = "!done"
-options = {option_help, option_done}
+msg_rows_error = "The number of rows should be greater than zero"
 
 formatter_title_type = "Formatter"
 text_title_type = "Text"
 level_title_type = "Level"
 label_title_type = "Label"
 url_title_type = "URL"
+rows_title_type = "Rows"
 title_types_msgs = {
     formatter_title_type: msg_formatter,
     text_title_type: msg_text,
     level_title_type: msg_level,
     label_title_type: msg_label,
-    url_title_type: msg_url
+    url_title_type: msg_url,
+    rows_title_type: msg_rows
 }
+
+option_help = "!help"
+option_done = "!done"
+options = {option_help, option_done}
+
+markdown = """"""
 
 
 def text_input(title_type):
@@ -40,6 +47,21 @@ def level_input():
             continue
         else:
             return level
+
+
+def row_input(row_number):
+    print(f'Row #{row_number}: ', end='')
+    return input()
+
+
+def rows_input():
+    while True:
+        print(msg_rows, end='')
+        rows = int(input())
+        if not rows > 0:
+            print(msg_rows_error)
+        else:
+            return rows
 
 
 def header_formatter():
@@ -80,6 +102,21 @@ def italic_formatter():
     return f'*{text}*'
 
 
+def list_formatter(ordered=True):
+    rows = rows_input()
+    string = ""
+    if markdown != """""":
+        string += '\n'
+    for i in range(1, rows + 1):
+        text = row_input(i)
+        if ordered:
+            string += f'{i}. {text}\n'
+        else:
+            string += f'* {text}\n'
+
+    return string
+
+
 formatters = {
     "plain": plain_formatter,
     "bold": bold_formatter,
@@ -87,7 +124,9 @@ formatters = {
     "header": header_formatter,
     "link": link_formatter,
     "inline-code": inline_code_formatter,
-    "new-line": new_line_formatter
+    "new-line": new_line_formatter,
+    "ordered-list": lambda: list_formatter(),
+    "unordered-list": lambda: list_formatter(ordered=False),
 }
 
 
@@ -95,7 +134,6 @@ def apply_formatter(formatter):
     return formatters[formatter]()
 
 
-markdown = """"""
 while True:
     choice = text_input(formatter_title_type)
     if choice in formatters:
